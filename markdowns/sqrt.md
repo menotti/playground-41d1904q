09cbaf64d1548ce1b60e4263e993fa8433a89203
Marchete <marchena.carlos@gmail.com>
2017-05-02 19:50:07 -0500

    Testing

--- markdowns/sqrt.md
+++ markdowns/sqrt.md
@@ -2,4 +2,4 @@
 
 SQRT Exercise
 
-@[Vectorized SQRT]({"stubs": ["sqrt/sqrt.cpp"], "command": "cd sqrt && ../mycompile.sh ./sqrt"})
+@[Vectorized SQRT]({"stubs": ["sqrt/sqrt.cpp"], "command": "ls && cd sqrt && ../mycompile.sh ./sqrt"})
--- projects/avx/prereq/detect.sh
+++ projects/avx/prereq/detect.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #CPU flag detection
 echo "****Getting CPU flag capabilities and number of cores"
-cat /proc/cpuinfo  | egrep "(flags|model|vendor)" | sort | uniq -c
+cat /proc/cpuinfo  | egrep "(flags|model name|vendor)" | sort | uniq -c
 #Compiler capabilities. -march=native is required!
 echo "****Getting GCC capabilities"
 gcc -march=native -dM -E - < /dev/null | egrep "SSE|AVX" | sort 
