6881595b4d7f754a038cdfee8d2a774719465df8
Marchete <marchena.carlos@gmail.com>
2017-05-22 18:17:08 -0500

    Test Autovectorization Verbose Log

--- markdowns/autovector.md
+++ markdowns/autovector.md
@@ -1,4 +1,4 @@
 # Automatic Vectorization
 
 GCC Autovectorization, enabled with -ftree-vectorize, enable verbose mode with -fopt-info-vec-optimized or -fopt-info-vec-all or -fopt-info-vec-missed
-@[Autovectorization]({"stubs": ["autovector/autovector.cpp"], "command": "./mycompile.sh autovector ./autovector"})
+@[Autovectorization]({"stubs": ["autovector/autovector.cpp","autovector/compilerflags.txt"], "command": "./mycompileLOG.sh autovector ./autovector"})
---
+++ projects/avx/mycompileLOG.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Compile
+cd $1
+compflags=`cat compilerflags.txt | head -n 1`
+echo "Compiler Flags are: $compflags"
+echo "cmake_minimum_required(VERSION 2.8.9)" > CMakeLists.txt
+echo 'set(CMAKE_CXX_FLAGS "$compflags")' >> CMakeLists.txt
+echo "add_executable($1 $1.cpp)" >> CMakeLists.txt
+cmake . > /tmp/cmake_logs 2>&1
+if make > /tmp/compilation_logs 2>&1; then
+    cat /tmp/compilation_logs
+	$2 $3 $4
+	if [ $? -eq 0 ]
+	then
+	  echo "CG> success true"
+	else
+	  echo "CG> success false"
+	fi	
+else
+	echo "CG> redirect-streams \"Compilation logs\""
+	cat /tmp/compilation_logs
+	echo "CG> success false"
+fi
