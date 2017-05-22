9d5b90cfde3e56f1c39bb5a16725dc10e1396719
Marchete <marchena.carlos@gmail.com>
2017-05-22 18:32:42 -0500

    Test Autovectorization Verbose Log

--- projects/avx/mycompileLOG.sh
+++ projects/avx/mycompileLOG.sh
@@ -4,7 +4,7 @@ cd $1
 compflags=`cat compilerflags.txt | head -n 1`
 echo "Compiler Flags are: $compflags"
 echo "cmake_minimum_required(VERSION 2.8.9)" > CMakeLists.txt
-echo 'set(CMAKE_CXX_FLAGS "$compflags")' >> CMakeLists.txt
+echo "set(CMAKE_CXX_FLAGS \"$compflags\")" >> CMakeLists.txt
 echo "add_executable($1 $1.cpp)" >> CMakeLists.txt
 cmake . > /tmp/cmake_logs 2>&1
 if make > /tmp/compilation_logs 2>&1; then
